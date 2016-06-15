8a0b68b0dc0fc1557d974465b07a2655fd4645c6
Yiming (Aaron) Zhang <awesome1aaron@gmail.com>
2016-06-15 16:27:32 -0400

    getting better
    I got this! now, i can theoretical properties. i am almost there!

--- Class_Build.py
+++ Class_Build.py
@@ -1,4 +1,4 @@
-import re
+import re, json
 try:
     import xml.etree.cElementTree as ET
 except:
@@ -6,6 +6,16 @@ except:
 
 
 class DrugBank:
+    def __init__(self):
+        self.ATC_dict = json.loads('all_level.json')
+        self.result = []
+        for event, drug = ET.iterparse('drugbank.xml'):
+            if drug.tag != '{http://www.drugbank.ca}drug':
+                continue
+            for first_level in drug:
+                if first_level.tag == '{http://www.drugbank.ca}calculated-properties':
+                    self.result += self.calculated_properties(first_level)
+
     def half_life(self, element_text): #element.tag == "{http://www.drugbank.ca}half-life"
         if element_text:
             content = re.findall(
@@ -35,9 +45,33 @@ class DrugBank:
     def protein_binding(self, element_text):
         pass
 
-    def predicted_proerties(self, element_text):
-        pass
-
     def type(self,element_itself):
         content = element_itself.attrib
         return content['type']
+
+    def ATC(self, *args):
+        hot_vector_position = []
+        for arg in args:
+            position = self.ATC_dict[arg]
+            hot_vector_position.append(position)
+        return hot_vector_position
+
+
+half_life = elem[15]
+atc_codes = elem[32]
+'{http://www.drugbank.ca}calculated-properties'
+experimental_properties = elem[39]
+
+level1 = atc_codes[0][1].attrib
+
+x.get("atrib's name") = attrib's value
+example:
+name = country.get('name')
+< country name = "Panama" >
+    < rank > 68 < / rank >
+    < year > 2011 < / year >
+    < gdppc > 13600 < / gdppc >
+    < neighbor name = "Costa Rica" direction = "W" / >
+    < neighbor name = "Colombia" direction = "E" / >
+< / country >
+
--- parseDrugBank.py
+++ parseDrugBank.py
@@ -10,74 +10,38 @@ try:
 except:
     import xml.etree.ElementTree as ET
 
-tree = ET.ElementTree(file= 'drugbank.xml')
-root = tree.getroot()
-
-# for elem in root[3].iter():
-#     print (elem)
-#     for i in elem:
-#         if i.tag=="{http://www.drugbank.ca}half-life":
-#             print (i.text)
+# tree = ET.ElementTree(file='drugbank.xml')
+# root = tree.getroot()
+# count = 0
+# for ii in range(3, 19):
+#     print(root[ii].attrib)
+    # count = 0
+    # for elem in root[ii].iter():
+    #
+    #     count += 1
+    #     if elem.attrib:
+    #         print(elem.attrib)
+    #         print(count)
+    #     if elem.tag=='{http://www.drugbank.ca}atc-codes':
+    #         for i in elem:
+    #             print(i.text)
+    #     if elem.tag == '{http://www.drugbank.ca}atc-code':
+    #         for j in elem:
+    #             print(j.text)
+#
+#     print()
 # for i in range(1,10):
 #     print (root[i][3].tag, root[i][3].text )
-    # this is the result:
-    # {http: // www.drugbank.ca}name
-    # Cetuximab
-    # {http: // www.drugbank.ca}name
-    # Dornase
-    # alfa
-    # {http: // www.drugbank.ca}name
-    # Denileukin
-    # diftitox
-    # {http: // www.drugbank.ca}name
-    # Etanercept
-    # {http: // www.drugbank.ca}name
-    # Bivalirudin
-    # {http: // www.drugbank.ca}name
-    # Leuprolide
-    # {http: // www.drugbank.ca}name
-    # Peginterferon
-    # alfa - 2
-    # a
-    # {http: // www.drugbank.ca}name
-    # Alteplase
-    # {http: // www.drugbank.ca}name
-    # Sermorelin
 
 
-for elements in root.iter():
-    count=0
-    for i in elements:
-        count+=1
-        if i.tag == "{http://www.drugbank.ca}half-life" and i.text:
-            content = re.findall(r'(\d+)((?: hours| mins| days| day| hrs| min| h| minutes| hour|\) hour|\) hours|h| Hrs| weeks| week| secs| Hours| seconds|\+ hours|\) minutes| months))', i.text)
-            if not content:
-                print (i.text)
-            for each_tuple_result in content:
-                digit = float(each_tuple_result[0])
-                unit = each_tuple_result[1]
-                if unit == " days" or unit==" day": #be careful, there's a space in front
-                    final_result = digit
-                elif unit == " mins" or unit==" min" or unit==" minutes" or unit==") minutes":
-                    final_result = digit/60.0
-                elif unit == " weeks" or unit == " week":
-                    final_result = digit*24*7
-                elif unit == " secs" or unit == " seconds":
-                    final_result = digit/3600
-                elif unit == " months":
-                    final_result = digit*24*30
-                else:
-                    final_result = digit*24
-                #print (final_result)
 
-            #
-            # num = list(map(int, content))
-            # if num:
-            #     avg = sum(num)/float(len(num))
-            #     print (avg)
-            # else:
-            #     print (i.text)
-# import re
-# l = "this is cool, 123hr or 5% or 0"
-# obj = re.findall(r'(\d+)(?:hr|%)', l)
-# print (obj)
+for whatever in range(3000):
+    event, elem = next(ET.iterparse('drugbank.xml'))
+
+    if elem.tag == '{http://www.drugbank.ca}drug':
+        flag = 0
+        for i in range(len(elem)):
+            if elem[i].tag== '{http://www.drugbank.ca}calculated-properties':
+                print (i)
+    else:
+        continue
