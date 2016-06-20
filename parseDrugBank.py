c077a64d089f5c689b156297fc3da42863aa841d
Yiming (Aaron) Zhang <awesome1aaron@gmail.com>
2016-06-20 17:40:56 -0400

    3 functions
    ATC level, type, half-life, experimental-properties

--- parseDrugBank.py
+++ parseDrugBank.py
@@ -9,39 +9,111 @@ try:
     import xml.etree.cElementTree as ET
 except:
     import xml.etree.ElementTree as ET
+import json
 
-# tree = ET.ElementTree(file='drugbank.xml')
-# root = tree.getroot()
-# count = 0
-# for ii in range(3, 19):
-#     print(root[ii].attrib)
-    # count = 0
-    # for elem in root[ii].iter():
-    #
-    #     count += 1
-    #     if elem.attrib:
-    #         print(elem.attrib)
-    #         print(count)
-    #     if elem.tag=='{http://www.drugbank.ca}atc-codes':
-    #         for i in elem:
-    #             print(i.text)
-    #     if elem.tag == '{http://www.drugbank.ca}atc-code':
-    #         for j in elem:
-    #             print(j.text)
-#
-#     print()
-# for i in range(1,10):
-#     print (root[i][3].tag, root[i][3].text )
+def half_life(first_level):
+    content = []
+    if first_level.text:
+        content = re.findall(r'(\d+)((?: hours| mins| days| day| hrs| min| h| minutes| hour|\) hour|\) hours|h| Hrs| weeks| week| secs| Hours| seconds|\+ hours|\) minutes| months))', first_level.text)
+    if not content:
+        return
+    for each_tuple_result in content:
+        digit = float(each_tuple_result[0])
+        unit = each_tuple_result[1]
+        if unit == " days" or unit == " day":  # be careful, there's a space in front
+            final_result = digit
+        elif unit == " mins" or unit == " min" or unit == " minutes" or unit == ") minutes":
+            final_result = digit / 60.0
+        elif unit == " weeks" or unit == " week":
+            final_result = digit * 24 * 7
+        elif unit == " secs" or unit == " seconds":
+            final_result = digit / 3600
+        elif unit == " months":
+            final_result = digit * 24 * 30
+        else:
+            final_result = digit * 24
+    return final_result
+    
+def level(first_level):
+    result = []
+    for child in first_level:
+        for level in child:
+            atc = level.text
+            if atc in d:
+                result.append(d[atc])
+    return result
 
 
+def experimental(first_level):
+    result = []
+    for ep in first_level: # experimental-properties (ep)
+        if ep[0].text=="Water Solubility":
+            ep_content = re.findall(r'(\d.+)((?: mg/mL| mg/L))', ep[1].text)
+            if not ep_content:
+                continue
+            try:
+                if ep_content[0][1]==" mg/L":
+                    result.append(float(ep_content[0][0])/1000)
+                else:
+                    result.append(float(ep_content[0][0]))
+            except:
+                pass
+        elif ep[0].text=="logP":
+            result.append(ep[1].text)
+        elif ep[0].text =="logS":
+            result.append(ep[1].text)
+        elif ep[0].text =="pKa":
+            result.append(ep[1].text)
+        elif ep[0].text == "Isoelectric Point":
+            result.append(ep[1].text)
+        elif ep[0].text == "Molecular Weight":
+            pass
+        elif ep[0].text == "Melting Point":
+            pass
+        else:
+            pass
+    return result
 
-for whatever in range(3000):
-    event, elem = next(ET.iterparse('drugbank.xml'))
 
-    if elem.tag == '{http://www.drugbank.ca}drug':
-        flag = 0
-        for i in range(len(elem)):
-            if elem[i].tag== '{http://www.drugbank.ca}calculated-properties':
-                print (i)
-    else:
+with open("output.txt") as f:
+    atc_dict= f.read()
+d = json.loads(atc_dict)
+count = 0
+flag = 0
+for event, drug in ET.iterparse('drugbank.xml'):
+    if drug.tag != '{http://www.drugbank.ca}drug':
         continue
+    if not drug.get('type'):
+        continue
+   
+    result = []
+    if drug.get('type')=='small molecule':
+        result.append(1)
+    else:
+        result.append(0)
+        
+    for first_level in drug:
+        h = 0
+        l=0
+        e=0
+        if first_level.tag == "{http://www.drugbank.ca}half-life":
+            h = half_life(first_level)
+       
+        if first_level.tag == '{http://www.drugbank.ca}atc-codes':
+            l = level(first_level)
+            
+        if first_level.tag == '{http://www.drugbank.ca}experimental-properties':
+            e = experimental(first_level)
+            
+        if h:
+            result.append(h)
+        if l:
+            result.append(l)
+        if e:
+            result.append(e)
+    if len(result)>=3:
+        count+=1
+print (count)
+    
+
+       
